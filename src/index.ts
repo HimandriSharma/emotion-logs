8bea91240828c03e6fc5db50e1329ecd7f544a2c
Himandri Sharma <himandrisharma27@gmail.com>
2022-06-06 16:47:05 +0530

    completed tests and added .npmignore

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,43 @@
-export function add(x:number,y:number): number{
-    return x+y;
+export function angry(input:String): void{
+    console.log(`\x1b[31m${input}`);
+}
+export function happy(input:String): void{
+    //light yellow
+    console.log(`\x1b[33m${input}`);
+}
+export function ecstatic(input:String): void{
+    //dark yellow
+    console.log(`\x1b[92m${input}`);
+}
+export function admiring(input:String): void{
+    //green yellow
+    console.log(`\x1b[92m\x1b[1m${input}`);
+}
+export function annoying(input:String): void{
+    //light red
+    console.log(`\x1b[41m${input}`);
+}
+export function boring(input:String): void{
+    //purple
+    console.log(`\x1b[35m${input}`);
+}
+export function disgusting(input:String): void{
+    //dark purple
+    console.log(`\x1b[105m${input}`);
+}
+export function sad(input:String): void{
+    //blue
+    console.log(`\x1b[44m${input}`);
+}
+export function surprising(input:String): void{
+    //cyan
+    console.log(`\x1b[96m${input}`);
+}
+export function fear(input:String): void{
+    //green
+    console.log(`\x1b[32m${input}`);
+}
+export function terrorizing(input:String): void{
+    //dim green
+    console.log(`\x1b[92m\x1b[2m${input}`);
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,58 @@
-import { add } from "../src/index";
-describe("test add function", () => {
-  it("should return 15 for add(10,5)", () => {
-    expect(add(10, 5)).toBe(15);
+import * as functions from "../src/index";
+describe("testing function", () => {
+  it('console.log the text "happy"', () => {
+    console.log = jest.fn();
+    functions.happy('happy')
+    expect(console.log).toHaveBeenCalledWith('\x1b[33mhappy');
   });
-it("should return 5 for add(2,3)", () => {
-    expect(add(2, 3)).toBe(5);
+  it('console.log the text "angry"', () => {
+    console.log = jest.fn();
+    functions.angry('angry')
+    expect(console.log).toHaveBeenCalledWith('\x1b[31mangry');
+  });
+  it('console.log the text "ecstatic"', () => {
+    console.log = jest.fn();
+    functions.ecstatic('ecstatic')
+    expect(console.log).toHaveBeenCalledWith('\x1b[92mecstatic');
+  });
+  it('console.log the text "admiring"', () => {
+    console.log = jest.fn();
+    functions.admiring('admiring')
+    expect(console.log).toHaveBeenCalledWith('\x1b[92m\x1b[1madmiring');
+  });
+  it('console.log the text "annoying"', () => {
+    console.log = jest.fn();
+    functions.annoying('annoying')
+    expect(console.log).toHaveBeenCalledWith('\x1b[41mannoying');
+  });
+  it('console.log the text "boring"', () => {
+    console.log = jest.fn();
+    functions.boring('boring')
+    expect(console.log).toHaveBeenCalledWith('\x1b[35mboring');
+  });
+  it('console.log the text "disgusting"', () => {
+    console.log = jest.fn();
+    functions.disgusting('disgusting')
+    expect(console.log).toHaveBeenCalledWith('\x1b[105mdisgusting');
+  });
+  it('console.log the text "sad"', () => {
+    console.log = jest.fn();
+    functions.sad('sad')
+    expect(console.log).toHaveBeenCalledWith('\x1b[44msad');
+  });
+  it('console.log the text "surprising"', () => {
+    console.log = jest.fn();
+    functions.surprising('surprising')
+    expect(console.log).toHaveBeenCalledWith('\x1b[96msurprising');
+  });
+  it('console.log the text "fear"', () => {
+    console.log = jest.fn();
+    functions.fear('fear')
+    expect(console.log).toHaveBeenCalledWith('\x1b[32mfear');
+  });
+  it('console.log the text "terrorizing"', () => {
+    console.log = jest.fn();
+    functions.terrorizing('terrorizing')
+    expect(console.log).toHaveBeenCalledWith('\x1b[92m\x1b[2mterrorizing');
   });
 });
